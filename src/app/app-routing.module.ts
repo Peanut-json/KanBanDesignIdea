c81340a933ad5dbdf31871893815de4e568964aa
Peanut-json <retroownerxd@gmail.com>
2023-06-21 15:59:29 +0100

    working with SCSS and HTML creating board

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainViewComponent } from './Pages/main-view/main-view.component';
 
-const routes: Routes = [
+const routes: Routes = [  // * using routing again to path to diffrent parts of the application, 
+                         //  * using pathing to the SPECIFIC area and then have it linked to the Component of choice.  
 {
   path: '' ,
   
